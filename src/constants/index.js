71b45f77998df38fb98c8e85919988a2df6dc727
ekenefidel <echukwurah99@gmail.com>
2024-05-31 11:09:57 +0100

    Add prayer hero implementation

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,3 +24,21 @@ export const navLinks = [
     label: "Donations",
   },
 ];
+
+export const sermons = [
+  {
+    date: "Feb 26, 2024",
+    title: "The love of God",
+    image: "/assets/images/sermon-img-3.svg",
+  },
+  {
+    date: "Feb 29, 2024",
+    title: "A God you can depend on",
+    image: "/assets/images/sermon-img-1.svg",
+  },
+  {
+    date: "April 6, 2024",
+    title: "How to remain faithful",
+    image: "/assets/images/sermon-img-2.svg",
+  },
+];
